2c7ac1dd0d3b7b6dcd3b4f26bceb3f093bee34d8
yuzawa <a@gmail.com>
2018-12-02 04:12:30 +0900

    プレイヤーを動かせて見た
    プレイヤーの移動、カメラの操作をしてみた

---
+++ Repuru/Assets/Script/PlayerCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    [SerializeField] private Transform player;   //追従対象プレイヤー
+
+    [SerializeField] private float distance = 15.0f; //追従対象プレイヤーから距離
+    [SerializeField] private Quaternion vRotation;  //カメラの垂直回転
+    [SerializeField] public Quaternion hRotation;   //カメラの水平回転
+
+
+	// Use this for initialization
+	void Start () {
+
+        vRotation = Quaternion.Euler(30, 0, 0);
+        hRotation = Quaternion.identity;
+        transform.rotation = hRotation * vRotation;
+
+        transform.position = player.position - transform.rotation * Vector3.forward * distance;
+
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.position - transform.rotation * Vector3.forward * distance;
+	}
+}
---
+++ Repuru/Assets/Script/PlayerContoroller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoroller : MonoBehaviour {
+    [SerializeField] private Vector3 velocity;          //移動方向
+    [SerializeField] private float movespeed = 5.0f;    //移動速度
+    [SerializeField] private float applySpeed = 0.2f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //velocityを
+        velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) {
+            velocity.z += 1;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            velocity.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            velocity.z -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            velocity.x += 1;
+        }
+
+        velocity = velocity.normalized * movespeed * Time.deltaTime;
+
+
+        if (velocity.magnitude > 0) {
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(-velocity),
+                                                  applySpeed);
+            transform.position += velocity;
+        }
+    }
+}
