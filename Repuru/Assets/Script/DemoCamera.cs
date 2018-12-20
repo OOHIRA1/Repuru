ddd877f0d5968b738eff340a9ce84c5bec02fe79
中條博道 <amezisutodragon@gmail.com>
2018-12-21 00:07:34 +0900

    自分のテストシーン

---
+++ Repuru/Assets/Script/DemoCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoCamera : MonoBehaviour {
+
+	GameObject Target;
+	Vector3 TargetPos;
+	 
+	void Start () {
+	    Target = GameObject.Find("Player");
+	    TargetPos = Target.transform.position;
+	}
+	 
+	void Update() {
+	    // targetの移動量分、自分（カメラ）も移動する
+	    transform.position += Target.transform.position - TargetPos;
+	    TargetPos = Target.transform.position;
+	 
+	    // マウスの右クリックを押している間
+	    if (Input.GetMouseButton(1)) {
+	        // マウスの移動量
+	        float MouseInputX = Input.GetAxis("Mouse X");
+	        float MouseInputY = Input.GetAxis("Mouse Y");
+	        // targetの位置のY軸を中心に、回転（公転）する
+	        transform.RotateAround(TargetPos, Vector3.up, MouseInputX * Time.deltaTime * 200f);
+	        // カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
+	        transform.RotateAround(TargetPos, transform.right, MouseInputY * Time.deltaTime * 200f);
+	    }
+	}  
+}
---
+++ Repuru/Assets/Script/DemoPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoPlayer : MonoBehaviour {
+
+	float InputHorizontal;
+	float InputVertical;
+	Rigidbody rb;
+ 
+	[SerializeField]float MoveSpeed;
+	 
+	void Start() {
+	    rb = GetComponent<Rigidbody>();
+	}
+	 
+	void Update() {
+	    InputHorizontal = Input.GetAxisRaw("Horizontal");
+	    InputVertical = Input.GetAxisRaw("Vertical");
+	}
+	 
+	void FixedUpdate() {
+	    // カメラの方向から、X-Z平面の単位ベクトルを取得
+	    Vector3 CameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+	 
+	    // 方向キーの入力値とカメラの向きから、移動方向を決定
+	    Vector3 MoveForward = CameraForward * InputVertical + Camera.main.transform.right * InputHorizontal;
+	 
+	    // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+	    rb.velocity = MoveForward * MoveSpeed + new Vector3(0, rb.velocity.y, 0);
+	 
+	    // キャラクターの向きを進行方向に
+	    if (MoveForward != Vector3.zero) {
+	        transform.rotation = Quaternion.LookRotation(MoveForward);
+	    }
+	}
+}
