e7eb9ea6801824f82d696d42e40d462a78137795
yuzawa <a@gmail.com>
2019-02-14 17:28:49 +0900

    TitleSceneの作成 StageSelectSceneの作成 ダミー画像の追加

---
+++ Repuru/Assets/Script/SceneTrasition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrasition : MonoBehaviour {
+
+
+	//TitleSceneに遷移する関数-----------------------------
+	public void TitleTrasition() {
+		SceneManager.LoadScene("Title");
+	}
+	//-----------------------------------------------------
+
+	//StageSelectSceneに遷移する関数-----------------------
+	public void StageSelectTrasition() {
+		SceneManager.LoadScene("StageSelect");
+	}
+	//-----------------------------------------------------
+
+	//MainSceneに遷移する関数-----------------------------
+	public void MainTrasition() {
+		SceneManager.LoadScene("Main");
+	}
+	//-----------------------------------------------------
+
+	//ResultSceneに遷移する関数----------------------------
+	public void ResultTrasition() {
+		SceneManager.LoadScene("Result");
+	}
+	//-----------------------------------------------------
+}
---
+++ Repuru/Assets/Script/TitleUi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleUi : MonoBehaviour {
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		
+	}
+
+	void Update () {
+		if (Input.GetKey("z")) {
+			TitleAnim ();
+		}
+	}
+
+	public void TitleAnim(){
+		animator.SetTrigger("Title");
+	}
+
+}
