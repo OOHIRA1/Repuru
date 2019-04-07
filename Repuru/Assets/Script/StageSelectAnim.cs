0462e73d2fcfb277a1f963393333651822c94d4b
DESKTOP-I70F9GA\raiwe <sdsjhsgd@gmail.com>
2019-04-07 14:57:57 +0900

    ステージ背景画像の追加、ステージセレクトのアニメーション作成

---
+++ Repuru/Assets/Script/StageSelectAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelectAnim : MonoBehaviour {
+    [SerializeField] GameObject backGround2 = null;
+    Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BackGround2Display() {
+        backGround2.SetActive(true);
+    }
+
+
+}
