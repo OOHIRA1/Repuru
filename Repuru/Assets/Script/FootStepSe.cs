ac12fa1722da9dce505aa0800dec4e345002c3dc
DESKTOP-I70F9GA\raiwe <sdsjhsgd@gmail.com>
2019-05-16 11:52:22 +0900

    ステージセレクトのアニメーションの更新

---
+++ Repuru/Assets/Script/FootStepSe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStepSe : MonoBehaviour
+{
+    public AudioClip footStep;
+    private AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = footStep;
+    }
+
+    public void SeStep(){
+        audioSource.Play();
+    }
+}
